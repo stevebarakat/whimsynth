2b3604c3f9a5eab8e92d4c98d456ee57c7a8413f
stevebarakat <stevebarakat@gmail.com>
2026-02-23 18:00:43 -0500

    ci

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import Synth from "@/components/Synth";
+import Footer from "@/components/Footer";
 import "@/styles/variables.css";
 
 function App() {
-  return <Synth />;
+  return (
+    <>
+      <Synth />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
